544537051d2854685dc0bff2a675b7929f841199
Rokas <rookas.rudzenskas@gmail.com>
2021-08-24 11:45:37 +0300

    Rokas

--- amplify/backend/function/cryptokingdomdf5b6cb2PostConfirmation/src/custom.js
+++ amplify/backend/function/cryptokingdomdf5b6cb2PostConfirmation/src/custom.js
@@ -20,7 +20,7 @@ exports.handler = async (event, context) => {
       'image': { S: event.request.userAttributes.picture },
       'createdAt': { S: date.toISOString() },
       'updatedAt': { S: date.toISOString() },
-      'netWorth': { D: 100000.0 },
+      'netWorth': { N: "100000.0" },
     },
     TableName: process.env.USERTABLE,
   }
