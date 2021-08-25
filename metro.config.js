f2670168cf60fdddd636d542e652be7842bb1de0
Rokas <rookas.rudzenskas@gmail.com>
2021-08-25 10:08:00 +0300

    Rokas

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
-const exclusionList  = require('metro-config/src/defaults/exclusionList');
-
 module.exports = {
     resolver: {
-        blacklistRE: exclusionList([/amplify\/#current-cloud-backend\/.*/]),
+        blacklistRE: /#current-cloud-backend\/.*/
     },
     transformer: {
         getTransformOptions: async () => ({
