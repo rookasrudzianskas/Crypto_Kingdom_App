2045d5d6743abbd6245faf432633470180ff81b4
Rokas <rookas.rudzenskas@gmail.com>
2021-08-29 15:03:04 +0300

    Rokas

--- amplify/backend/function/cryptoKindomSubmitOrder/src/index.js
+++ amplify/backend/function/cryptoKindomSubmitOrder/src/index.js
@@ -102,7 +102,7 @@ const buyCoin = async(
             '__typename': { S: 'PortfolioCoin' },
             'createdAt': { S: date.toISOString() },
             'updatedAt': { S: date.toISOString() },
-            'userId': { S: userId },
+            'userId': { S: userId.toString() },
             'coinId': { S: process.env.USD_COIN_ID },
             'amount': { N: newUsdAmount.toString() }
         },
@@ -116,11 +116,11 @@ const buyCoin = async(
     const newCoinAmount = coinAmount + amountToBuy;
     const params1 = {
         Item: {
-            id: { S: `${userId}-${coin.Item.symbol.S}` },
+            id: { S: `${userId.toString()}-${coin.Item.symbol.S}` },
             '__typename': { S: 'PortfolioCoin' },
             'createdAt': { S: date.toISOString() },
             'updatedAt': { S: date.toISOString() },
-            'userId': { S: userId },
+            'userId': { S: userId.toString() },
             'coinId': { S: coin.Item.id.S },
             'amount': { N: newCoinAmount.toString() }
         },
@@ -147,7 +147,7 @@ const sellCoin = async(
             '__typename': { S: 'PortfolioCoin' },
             'createdAt': { S: date.toISOString() },
             'updatedAt': { S: date.toISOString() },
-            'userId': { S: userId },
+            'userId': { S: userId.toString() },
             'coinId': { S: process.env.USD_COIN_ID },
             'amount': { N: newUsdAmount.toString() }
         },
@@ -161,11 +161,11 @@ const sellCoin = async(
     const newCoinAmount = coinAmount - amountToSell;
     const params1 = {
         Item: {
-            id: { S: `${userId}-${coin.Item.symbol.S}` },
+            id: { S: `${userId.toString()}-${coin.Item.symbol.S}` },
             '__typename': { S: 'PortfolioCoin' },
             'createdAt': { S: date.toISOString() },
             'updatedAt': { S: date.toISOString() },
-            'userId': { S: userId },
+            'userId': { S: userId.toString() },
             'coinId': { S: coin.Item.id.S },
             'amount': { N: newCoinAmount.toString() }
         },
