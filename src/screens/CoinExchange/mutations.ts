c1a89b95d59133b2820cd41c0fdf22fdb1cb16a2
Rokas <rookas.rudzenskas@gmail.com>
2021-08-29 18:40:02 +0300

    Rokas

---
+++ src/screens/CoinExchange/mutations.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const exchangeCoins = /* GraphQL */ `
+    mutation ExchangeCoins(
+        $coinId: ID!
+        $isBuy: Boolean!
+        $amount: Float!
+        $usdPortfolioCoinId: ID
+        $coinPortfolioCoinId: ID
+    ) {
+        exchangeCoins(
+            coinId: $coinId
+            isBuy: $isBuy
+            amount: $amount
+            usdPortfolioCoinId: $usdPortfolioCoinId
+            coinPortfolioCoinId: $coinPortfolioCoinId
+        )
+    }
+`;
