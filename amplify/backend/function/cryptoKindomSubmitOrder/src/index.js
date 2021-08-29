e66d83ae5991b2496791b7b7e9a7549186981b60
Rokas <rookas.rudzenskas@gmail.com>
2021-08-29 15:34:55 +0300

    Rokas

--- amplify/backend/function/cryptoKindomSubmitOrder/src/index.js
+++ amplify/backend/function/cryptoKindomSubmitOrder/src/index.js
@@ -3,72 +3,54 @@
 	API_CRYPTOKINGDOM_GRAPHQLAPIIDOUTPUT
 Amplify Params - DO NOT EDIT */
 
-const { CognitoIdentityServiceProvider,  DynamoDB} = require('aws-sdk');
-// const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+
+
+const { DynamoDB } = require('aws-sdk');
+
 const ddb = new DynamoDB();
-
-// /**
-//  * Get user pool information from environment variables.
-//  */
-//
-// const COGNITO_USERPOOL_ID = process.env.COGNITO_USERPOOL_ID;
-// if (!COGNITO_USERPOOL_ID) {
-//     throw new Error(`Function requires environment variable: 'COGNITO_USERPOOL_ID'`);
-// }
-// const COGNITO_USERNAME_CLAIM_KEY = 'cognito:username';
-
-
 
 const getCoinAmount = async (coinPortfolioCoinId, userId) => {
     const params = {
         Key: {
             id: { S: coinPortfolioCoinId },
         },
-        TableName: process.env.PORTFOLIO_COIN_TABLE,
+        TableName: process.env.PORTFOLIO_COIN_TABLE
     }
     const coinData = await ddb.getItem(params).promise();
-    console.log('portfolio coin data');
+    console.log('porftolio coin data');
     console.log(coinData);
-    // @TODO check if it is indeed the coin and belongs to the user
-    // userId: { S: userId },
-
-    if(coinData && coinData.Item && coinData.Item.amount && coinData.Item.amount.N) {
-        return coinData.Item.amount.N;
+    // TOdo CHECK if it is indeed coin, and belongs to user
+    if (coinData && coinData.Item && coinData.Item.amount && coinData.Item.amount.N) {
+        return parseFloat(coinData.Item.amount.N);
     }
     return 0;
-    // return coinData?.Item?.amount?.N || 0;
 }
-
 
 const getUsdAmount = async (usdPortfolioCoinId, userId) => {
     const params = {
         Key: {
-            id: { S: usdPortfolioCoinId },
+            id: { S: usdPortfolioCoinId }
         },
-        TableName: process.env.PORTFOLIO_COIN_TABLE,
+        TableName: process.env.PORTFOLIO_COIN_TABLE
     }
     const coinData = await ddb.getItem(params).promise();
     console.log('usd coin data');
     console.log(coinData);
-
-    // @TODO check if it is indeed the usd coin and belongs to the user
-
+    // TOdo CHECK if it is indeed USD coin, and belongs to user
     // coinId: { S: process.env.USD_COIN_ID },
     // userId: { S: userId },
-    if(coinData && coinData.Item && coinData.Item.amount && coinData.Item.amount.N) {
-        return coinData.Item.amount.N;
+    if (coinData && coinData.Item && coinData.Item.amount && coinData.Item.amount.N) {
+        return parseFloat(coinData.Item.amount.N);
     }
     return 0;
-    // return coinData?.Item?.amount?.N || 0;
-
 }
 
-const getCoin = async (coinId)  => {
+const getCoin = async (coinId) => {
     const params = {
         Key: {
-          id: { S: coinId }
+            id: { S: coinId }
         },
-        TableName: process.env.COIN_TABLE,
+        TableName: process.env.COIN_TABLE
     }
     const coinData = await ddb.getItem(params).promise();
     console.log('coin data');
@@ -76,43 +58,43 @@ const getCoin = async (coinId)  => {
     return coinData;
 }
 
-
 const canBuyCoin = (coin, amountToBuy, usdAmount) => {
-    return usdAmount >= coin.Item.currentPrice.N * amountToBuy
+    console.log('can buycoin')
+    console.log(usdAmount)
+    console.log(coin.Item.currentPrice.N)
+    console.log(amountToBuy)
+    return usdAmount >= parseFloat(coin.Item.currentPrice.N) * amountToBuy
 }
 
 const canSellCoin = (amountToSell, portfolioAmount) => {
     return portfolioAmount >= amountToSell
 }
 
-const buyCoin = async(
+const buyCoin = async (
     coin,
     amountToBuy,
     usdPortfolioCoinId,
+    usdAmount,
     coinAmount,
-    userId,
-    usdAmount
-) => {
+    userId) => {
     const date = new Date();
-    // decrease USD to buy coin
-    const newUsdAmount = usdAmount - coin.Item.currentPrice.N * amountToBuy;
+    // decrease USD
+    const newUsdAmount = usdAmount - parseFloat(coin.Item.currentPrice.N) * amountToBuy;
     const params = {
         Item: {
             id: { S: usdPortfolioCoinId },
             '__typename': { S: 'PortfolioCoin' },
             'createdAt': { S: date.toISOString() },
             'updatedAt': { S: date.toISOString() },
-            'userId': { S: userId.toString() },
+            'userId': { S: userId },
             'coinId': { S: process.env.USD_COIN_ID },
             'amount': { N: newUsdAmount.toString() }
         },
-        TableName: process.env.PORTFOLIO_COIN_TABLE,
+        TableName: process.env.PORTFOLIO_COIN_TABLE
     }
     await ddb.putItem(params).promise();
 
-    // add new portfolio coin, or update the existing one
-
-
+    // ADD new portfolio coin, or update the existing one
     const newCoinAmount = coinAmount + amountToBuy;
     const params1 = {
         Item: {
@@ -124,23 +106,21 @@ const buyCoin = async(
             'coinId': { S: coin.Item.id.S },
             'amount': { N: newCoinAmount.toString() }
         },
-        TableName: process.env.PORTFOLIO_COIN_TABLE,
+        TableName: process.env.PORTFOLIO_COIN_TABLE
     }
     await ddb.putItem(params1).promise();
-
 }
 
-const sellCoin = async(
+const sellCoin = async (
     coin,
     amountToSell,
     usdPortfolioCoinId,
+    usdAmount,
     coinAmount,
-    userId,
-    usdAmount
-) => {
+    userId) => {
     const date = new Date();
     // increase USD
-    const newUsdAmount = usdAmount + coin.Item.currentPrice.N * amountToSell;
+    const newUsdAmount = usdAmount + parseFloat(coin.Item.currentPrice.N) * amountToSell;
     const params = {
         Item: {
             id: { S: usdPortfolioCoinId },
@@ -151,13 +131,11 @@ const sellCoin = async(
             'coinId': { S: process.env.USD_COIN_ID },
             'amount': { N: newUsdAmount.toString() }
         },
-        TableName: process.env.PORTFOLIO_COIN_TABLE,
+        TableName: process.env.PORTFOLIO_COIN_TABLE
     }
     await ddb.putItem(params).promise();
 
-    // add new portfolio coin, or update the existing one
-
-
+    // ADD new portfolio coin, or update the existing one
     const newCoinAmount = coinAmount - amountToSell;
     const params1 = {
         Item: {
@@ -169,21 +147,18 @@ const sellCoin = async(
             'coinId': { S: coin.Item.id.S },
             'amount': { N: newCoinAmount.toString() }
         },
-        TableName: process.env.PORTFOLIO_COIN_TABLE,
+        TableName: process.env.PORTFOLIO_COIN_TABLE
     }
     await ddb.putItem(params1).promise();
-
 }
-
 
 /**
  * Using this as the entry point, you can use a single function to handle many resolvers.
  */
-
 const resolvers = {
     Mutation: {
         exchangeCoins: async ctx => {
-            console.log('ctx')
+            console.log('ctx');
             console.log(ctx);
             const {
                 coinId,
@@ -194,23 +169,8 @@ const resolvers = {
             } = ctx.arguments;
             const userId = ctx.identity.sub;
 
-            const usdAmount = !usdPortfolioCoinId ? 0 : await getUsdAmount(usdPortfolioCoinId, userId);
-            const coinAmount = !coinPortfolioCoinId ? 0 : await getCoinAmount(coinPortfolioCoinId, userId);
-            // const params = {
-            //     UserPoolId: COGNITO_USERPOOL_ID, /* required */
-            //     Username: ctx.identity.claims[COGNITO_USERNAME_CLAIM_KEY], /* required */
-            // };
-            // try {
-            //     // Read more: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentityServiceProvider.html#adminGetUser-property
-            //     const userResponse = await cognitoIdentityServiceProvider.adminGetUser(params).promise();
-            //     console.log(userResponse);
-            // } catch (e) {
-            //     console.log(e);
-            //     // throw new Error(`NOT FOUND`);
-            // }
-
-            // const usdAmount = !usdPortfolioCoinId ? 0 : await getUsdAmount(usdPortfolioCoinId, userId);
-            // const coinAmount = !coinPortfolioCoinId ? 0 : await getCoinAmount(coinPortfolioCoinId, userId);
+            const usdAmount = !usdPortfolioCoinId ? 0 : await getUsdAmount(usdPortfolioCoinId, userId)
+            const coinAmount = !coinPortfolioCoinId ? 0 : await getCoinAmount(coinPortfolioCoinId, userId)
             const coin = await getCoin(coinId);
 
             try {
@@ -232,16 +192,6 @@ const resolvers = {
     },
 }
 
-// event
-// {
-//   "typeName": "Query", /* Filled dynamically based on @function usage location */
-//   "fieldName": "me", /* Filled dynamically based on @function usage location */
-//   "arguments": { /* GraphQL field arguments via $ctx.arguments */ },
-//   "identity": { /* AppSync identity object via $ctx.identity */ },
-//   "source": { /* The object returned by the parent resolver. E.G. if resolving field 'Post.comments', the source is the Post object. */ },
-//   "request": { /* AppSync request object. Contains things like headers. */ },
-//   "prev": { /* If using the built-in pipeline resolver support, this contains the object returned by the previous function. */ },
-// }
 exports.handler = async (event) => {
     const typeHandler = resolvers[event.typeName];
     if (typeHandler) {
@@ -252,3 +202,254 @@ exports.handler = async (event) => {
     }
     throw new Error("Resolver not found.");
 };
+
+
+// const { CognitoIdentityServiceProvider,  DynamoDB} = require('aws-sdk');
+// // const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+// const ddb = new DynamoDB();
+//
+// // /**
+// //  * Get user pool information from environment variables.
+// //  */
+// //
+// // const COGNITO_USERPOOL_ID = process.env.COGNITO_USERPOOL_ID;
+// // if (!COGNITO_USERPOOL_ID) {
+// //     throw new Error(`Function requires environment variable: 'COGNITO_USERPOOL_ID'`);
+// // }
+// // const COGNITO_USERNAME_CLAIM_KEY = 'cognito:username';
+//
+//
+//
+// const getCoinAmount = async (coinPortfolioCoinId, userId) => {
+//     const params = {
+//         Key: {
+//             id: { S: coinPortfolioCoinId },
+//         },
+//         TableName: process.env.PORTFOLIO_COIN_TABLE,
+//     }
+//     const coinData = await ddb.getItem(params).promise();
+//     console.log('portfolio coin data');
+//     console.log(coinData);
+//     // @TODO check if it is indeed the coin and belongs to the user
+//     // userId: { S: userId },
+//
+//     if(coinData && coinData.Item && coinData.Item.amount && coinData.Item.amount.N) {
+//         return coinData.Item.amount.N;
+//     }
+//     return 0;
+//     // return coinData?.Item?.amount?.N || 0;
+// }
+//
+//
+// const getUsdAmount = async (usdPortfolioCoinId, userId) => {
+//     const params = {
+//         Key: {
+//             id: { S: usdPortfolioCoinId },
+//         },
+//         TableName: process.env.PORTFOLIO_COIN_TABLE,
+//     }
+//     const coinData = await ddb.getItem(params).promise();
+//     console.log('usd coin data');
+//     console.log(coinData);
+//
+//     // @TODO check if it is indeed the usd coin and belongs to the user
+//
+//     // coinId: { S: process.env.USD_COIN_ID },
+//     // userId: { S: userId },
+//     if(coinData && coinData.Item && coinData.Item.amount && coinData.Item.amount.N) {
+//         return coinData.Item.amount.N;
+//     }
+//     return 0;
+//     // return coinData?.Item?.amount?.N || 0;
+//
+// }
+//
+// const getCoin = async (coinId)  => {
+//     const params = {
+//         Key: {
+//           id: { S: coinId }
+//         },
+//         TableName: process.env.COIN_TABLE,
+//     }
+//     const coinData = await ddb.getItem(params).promise();
+//     console.log('coin data');
+//     console.log(coinData);
+//     return coinData;
+// }
+//
+//
+// const canBuyCoin = (coin, amountToBuy, usdAmount) => {
+//     return usdAmount >= coin.Item.currentPrice.N * amountToBuy
+// }
+//
+// const canSellCoin = (amountToSell, portfolioAmount) => {
+//     return portfolioAmount >= amountToSell
+// }
+//
+// const buyCoin = async(
+//     coin,
+//     amountToBuy,
+//     usdPortfolioCoinId,
+//     coinAmount,
+//     userId,
+//     usdAmount
+// ) => {
+//     const date = new Date();
+//     // decrease USD to buy coin
+//     const newUsdAmount = usdAmount - coin.Item.currentPrice.N * amountToBuy;
+//     const params = {
+//         Item: {
+//             id: { S: usdPortfolioCoinId },
+//             '__typename': { S: 'PortfolioCoin' },
+//             'createdAt': { S: date.toISOString() },
+//             'updatedAt': { S: date.toISOString() },
+//             'userId': { S: userId.toString() },
+//             'coinId': { S: process.env.USD_COIN_ID },
+//             'amount': { N: newUsdAmount.toString() }
+//         },
+//         TableName: process.env.PORTFOLIO_COIN_TABLE,
+//     }
+//     await ddb.putItem(params).promise();
+//
+//     // add new portfolio coin, or update the existing one
+//
+//
+//     const newCoinAmount = coinAmount + amountToBuy;
+//     const params1 = {
+//         Item: {
+//             id: { S: `${userId}-${coin.Item.symbol.S}` },
+//             '__typename': { S: 'PortfolioCoin' },
+//             'createdAt': { S: date.toISOString() },
+//             'updatedAt': { S: date.toISOString() },
+//             'userId': { S: userId },
+//             'coinId': { S: coin.Item.id.S },
+//             'amount': { N: newCoinAmount.toString() }
+//         },
+//         TableName: process.env.PORTFOLIO_COIN_TABLE,
+//     }
+//     await ddb.putItem(params1).promise();
+//
+// }
+//
+// const sellCoin = async(
+//     coin,
+//     amountToSell,
+//     usdPortfolioCoinId,
+//     coinAmount,
+//     userId,
+//     usdAmount
+// ) => {
+//     const date = new Date();
+//     // increase USD
+//     const newUsdAmount = usdAmount + coin.Item.currentPrice.N * amountToSell;
+//     const params = {
+//         Item: {
+//             id: { S: usdPortfolioCoinId },
+//             '__typename': { S: 'PortfolioCoin' },
+//             'createdAt': { S: date.toISOString() },
+//             'updatedAt': { S: date.toISOString() },
+//             'userId': { S: userId },
+//             'coinId': { S: process.env.USD_COIN_ID },
+//             'amount': { N: newUsdAmount.toString() }
+//         },
+//         TableName: process.env.PORTFOLIO_COIN_TABLE,
+//     }
+//     await ddb.putItem(params).promise();
+//
+//     // add new portfolio coin, or update the existing one
+//
+//
+//     const newCoinAmount = coinAmount - amountToSell;
+//     const params1 = {
+//         Item: {
+//             id: { S: `${userId}-${coin.Item.symbol.S}` },
+//             '__typename': { S: 'PortfolioCoin' },
+//             'createdAt': { S: date.toISOString() },
+//             'updatedAt': { S: date.toISOString() },
+//             'userId': { S: userId },
+//             'coinId': { S: coin.Item.id.S },
+//             'amount': { N: newCoinAmount.toString() }
+//         },
+//         TableName: process.env.PORTFOLIO_COIN_TABLE,
+//     }
+//     await ddb.putItem(params1).promise();
+//
+// }
+//
+//
+// /**
+//  * Using this as the entry point, you can use a single function to handle many resolvers.
+//  */
+//
+// const resolvers = {
+//     Mutation: {
+//         exchangeCoins: async ctx => {
+//             console.log('ctx')
+//             console.log(ctx);
+//             const {
+//                 coinId,
+//                 isBuy,
+//                 amount,
+//                 usdPortfolioCoinId,
+//                 coinPortfolioCoinId,
+//             } = ctx.arguments;
+//             const userId = ctx.identity.sub;
+//
+//             const usdAmount = !usdPortfolioCoinId ? 0 : await getUsdAmount(usdPortfolioCoinId, userId);
+//             const coinAmount = !coinPortfolioCoinId ? 0 : await getCoinAmount(coinPortfolioCoinId, userId);
+//             // const params = {
+//             //     UserPoolId: COGNITO_USERPOOL_ID, /* required */
+//             //     Username: ctx.identity.claims[COGNITO_USERNAME_CLAIM_KEY], /* required */
+//             // };
+//             // try {
+//             //     // Read more: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentityServiceProvider.html#adminGetUser-property
+//             //     const userResponse = await cognitoIdentityServiceProvider.adminGetUser(params).promise();
+//             //     console.log(userResponse);
+//             // } catch (e) {
+//             //     console.log(e);
+//             //     // throw new Error(`NOT FOUND`);
+//             // }
+//
+//             // const usdAmount = !usdPortfolioCoinId ? 0 : await getUsdAmount(usdPortfolioCoinId, userId);
+//             // const coinAmount = !coinPortfolioCoinId ? 0 : await getCoinAmount(coinPortfolioCoinId, userId);
+//             const coin = await getCoin(coinId);
+//
+//             try {
+//                 if (isBuy && canBuyCoin(coin, amount, usdAmount)) {
+//                     await buyCoin(coin, amount, usdPortfolioCoinId, usdAmount, coinAmount, userId);
+//                 }
+//                 else if (!isBuy && canSellCoin(amount, coinAmount)) {
+//                     await sellCoin(coin, amount, usdPortfolioCoinId, usdAmount, coinAmount, userId);
+//                 } else {
+//                     throw new Error(isBuy ? `Not enough USD` : 'Not enough coins to sell');
+//                 }
+//             } catch (e) {
+//                 console.log(e);
+//                 throw new Error('Unexpected Error exchanging coins');
+//             }
+//
+//             return true;
+//         }
+//     },
+// }
+//
+// // event
+// // {
+// //   "typeName": "Query", /* Filled dynamically based on @function usage location */
+// //   "fieldName": "me", /* Filled dynamically based on @function usage location */
+// //   "arguments": { /* GraphQL field arguments via $ctx.arguments */ },
+// //   "identity": { /* AppSync identity object via $ctx.identity */ },
+// //   "source": { /* The object returned by the parent resolver. E.G. if resolving field 'Post.comments', the source is the Post object. */ },
+// //   "request": { /* AppSync request object. Contains things like headers. */ },
+// //   "prev": { /* If using the built-in pipeline resolver support, this contains the object returned by the previous function. */ },
+// // }
+// exports.handler = async (event) => {
+//     const typeHandler = resolvers[event.typeName];
+//     if (typeHandler) {
+//         const resolver = typeHandler[event.fieldName];
+//         if (resolver) {
+//             return await resolver(event);
+//         }
+//     }
+//     throw new Error("Resolver not found.");
+// };
