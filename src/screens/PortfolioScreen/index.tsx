222a45be73f49fcb4e91fc1c29aee6ecf84486da
Rokas <rookas.rudzenskas@gmail.com>
2021-08-30 07:29:49 +0300

    Rokas

--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -9,36 +9,8 @@ import {API, graphqlOperation} from "aws-amplify";
 import {getUserPortfolio} from "./queries";
 import App from "../../../App";
 import AppContext from "../../utils/AppContext";
+import NumberFormat from 'react-number-format';
 
-// const portfolioCoins = [{
-//     id: '1',
-//     name: 'Virtual Dollars',
-//     symbol: 'USD',
-//     amount: 79.993,
-//     valueUSD: 79.993,
-//     image: 'https://bitcoin.org/img/icons/opengraph.png?1628351347',
-// }, {
-//     id: '2',
-//     name: 'Bitcoin',
-//     symbol: 'BTC',
-//     image: 'https://bitcoin.org/img/icons/opengraph.png?1628351347',
-//     amount: 90.993,
-//     valueUSD: 90.993,
-// }, {
-//     id: '3',
-//     name: 'Eutherium',
-//     symbol: 'ETH',
-//     amount: 100.993,
-//     valueUSD: 100.993,
-//     image: 'https://bitcoin.org/img/icons/opengraph.png?1628351347',
-// }, {
-//     id: '4',
-//     name: 'Eutherium',
-//     symbol: 'ETH',
-//     amount: 100.993,
-//     valueUSD: 100.993,
-//     image: 'https://bitcoin.org/img/icons/opengraph.png?1628351347',
-// }];
 
 
 
@@ -58,7 +30,7 @@ const PortfolioScreen = (props) => {
             }))
 
             // @ts-ignore
-            console.log(response.data.getUser);
+            // console.log(response.data.getUser);
             // @ts-ignore
             setBalance(response.data.getUser.networth);
             // @ts-ignore
@@ -85,7 +57,9 @@ const PortfolioScreen = (props) => {
                     <View style={[tw`mt-12`]}>
                         <Text style={tw`text-3xl text-gray-200 font-bold`}>Portfolio balance</Text>
                         <View style={tw`mt-2`}>
-                            <Text style={tw`text-3xl text-white font-bold`}>$ <Text style={tw`text-4xl font-extrabold text-green-500 mt-5 text-center`}>{balance}</Text></Text>
+                            <Text style={tw`text-3xl text-white font-bold`}>$ <Text style={tw`text-4xl font-extrabold text-green-500 mt-5 text-center`}>
+                                <NumberFormat thousandSeparator={true} prefix={'$'} />
+                            </Text></Text>
                         </View>
                     </View>
                 </View>
