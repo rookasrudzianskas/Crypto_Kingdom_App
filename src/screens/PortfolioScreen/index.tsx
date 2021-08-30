fa8ced785aaf4267573e99692764e4b29e090dc4
Rokas <rookas.rudzenskas@gmail.com>
2021-08-30 07:35:49 +0300

    Rokas

--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -9,7 +9,7 @@ import {API, graphqlOperation} from "aws-amplify";
 import {getUserPortfolio} from "./queries";
 import App from "../../../App";
 import AppContext from "../../utils/AppContext";
-var NumberFormat = require('react-number-format');
+import NumberFormat from 'react-number-format';
 
 
 
